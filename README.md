6115d0e1cb85652a8cb90b8e048fa0ef5a581cc0
Eddy Jiang <eddyjiang55@outlook.com>
2026-02-02 19:22:40 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Markov Chain Discord Bot
+
+Discord bot to generate text based on previous chat messages using a Markov chain. Idea inspired by [nMarkov](https://nmarkov.xyz/).
---
+++ src/main.py
@@ -0,0 +1,14 @@
+import os
+import discord
+import random
+
+
+class MarkovChain:
+    def __init__(self):
+        pass
+
+    def train(self, message):
+        pass
+
+    def generate(self):
+        pass
